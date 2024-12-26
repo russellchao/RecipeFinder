dabb3f2168e84b63843ee7d2f6526596a9ec0a0d
russellchao <russchaoman@gmail.com>
2024-12-26 18:47:44 -0500

    Created Category and Choice models

--- recipefinder/models.py
+++ recipefinder/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    question_text = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.question_text
+    
+
+class Choice(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=30)
+    selected = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.choice_text
--- recipefinder/views.py
+++ recipefinder/views.py
@@ -4,10 +4,15 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
+from django.template import loader
 
 # from .models import ...
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Recipe Finder Homepage")
+    template = loader.get_template("recipefinder/index.html")
+    context = {
+        "message": "Hello!"
+    }
+    return HttpResponse(template.render(context, request))
