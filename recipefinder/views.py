eb56ba6e697efcf4610e3531bbead02e5f8fd4ee
russellchao <russchaoman@gmail.com>
2025-01-01 22:02:50 -0500

    Polished API calls to get more reliable results

--- recipefinder/views.py
+++ recipefinder/views.py
@@ -12,17 +12,13 @@ import requests
 
 API_KEY = 'ef8163f9b5d64b61a449552724d38c15'
 
-# Create your views here.
 
 def index(request):
     return render(request, "recipefinder/index.html")
     
 
 def preferences(request):
-    # Reset the selected variable of every Choice model to False
     Choice.objects.filter(selected=True).update(selected=False)
-
-    # Fetch all categories to display in the template
     categories = Category.objects.all()
     return render(request, 'recipefinder/preferences.html', {'categories': categories})
 
@@ -30,21 +26,21 @@ def preferences(request):
 def results(request):
     if request.method == "POST":
         selected_choice_ids = request.POST.getlist("choices")
-
-        # Reset all choices to not selected
         Choice.objects.all().update(selected=False)
-
-        # Update only the selected choices
         Choice.objects.filter(id__in=selected_choice_ids).update(selected=True)
-
-        # Gather the selected preferences
         selected_choices = Choice.objects.filter(selected=True)
-        ingredients = ",".join([choice.choice_text for choice in selected_choices])
+               
+        # Group choices based on category
+        choice_cat = dict()
+        for choice in selected_choices:
+            category_name = choice.category.category_text
+            if category_name not in choice_cat:
+                choice_cat[category_name] = ""
+            choice_cat[category_name] += choice.choice_text + ","
 
-        # Call the Spoonacular API
-        url = f"https://api.spoonacular.com/recipes/findByIngredients?ingredients={ingredients}&number=5&apiKey={API_KEY}"
+        url = f"https://api.spoonacular.com/recipes/complexSearch?cuisine={choice_cat["Cuisine"]}&number=15&addRecipeNutrition=true&apiKey={API_KEY}"
         response = requests.get(url)
-        recipes = response.json() if response.status_code == 200 else []
+        recipes = response.json().get('results', []) if response.status_code == 200 else []
 
         return render(request, "recipefinder/results.html", {"recipes": recipes})
 
